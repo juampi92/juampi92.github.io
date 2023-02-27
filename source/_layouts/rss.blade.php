080cd8bb1cb649a71ba3087bbc24b78e2376e6a5
Juan Pablo Barreto <juampi92@gmail.com>
2023-02-27 19:58:55 +0100

    fix: rss path
    Remove /blog path
    Remove trailing `/` on feed.atom

--- source/_layouts/rss.blade.php
+++ source/_layouts/rss.blade.php
@@ -1,10 +1,10 @@
 <?php echo "<?xml version=\"1.0\"?>\n"; ?>
 <feed xmlns="http://www.w3.org/2005/Atom">
     <title>{{ $page->siteName }}</title>
-    <link href="{{ rightTrimPath($page->baseUrl) }}/{{ $page->site_path }}" />
-    <link type="application/atom+xml" rel="self" href="{{ $page->getUrl() }}/" />
+    <link href="{{ rightTrimPath($page->baseUrl) }}/" />
+    <link type="application/atom+xml" rel="self" href="{{ $page->getUrl() }}" />
     <updated>{{ date(DATE_ATOM) }}</updated>
-    <id>{{ $page->getUrl() }}/</id>
+    <id>{{ $page->getUrl() }}</id>
     <author>
         <name>{{ $page->siteAuthor }}</name>
     </author>
